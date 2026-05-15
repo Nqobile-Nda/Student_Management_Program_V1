9f968c072d5c205ee77052c47c59efaaeb440203
Nqobile-Nda <ndabezithanqobile27@gmail.com>
2026-05-15 16:10:31 +0200

    Refactor grade_table to ensure unique grades and add student count column

--- models/grade.py
+++ models/grade.py
@@ -11,15 +11,29 @@ def grade_table():
     cur.execute("""
     CREATE TABLE IF NOT EXISTS grades_table (
     grade_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    grade TEXT NOT NULL
+    grade TEXT NOT NULL UNIQUE,
+    grade_student_count TEXT NOT NULL
     );
     """)
 
+
     grades = [8, 9, 10, 11, 12]
     for grade in grades:
+
         cur.execute("""
-        IF NOT EXISTS grades_table INSERT INTO grades_table (grade) VALUES (?)
+        INSERT OR IGNORE INTO grades_table (grade) VALUES (?)
         """, (grade,))
 
     conn.commit()
     conn.close()
+
+
+def load_grades():
+    conn, cur = database_connection()
+    cur.execute("""
+    SELECT * FROM grades_table
+    """)
+    grade_options = [dict(row) for row in cur.fetchall()]
+
+    conn.close()
+    return grade_options
